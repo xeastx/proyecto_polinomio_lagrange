ea9f0a9ec28b79b78488b5d2e0f782068a8c5208
Enrique Salazar <xeast98@gmail.com>
2019-11-21 11:10:45 -0400

    Update Enrique_27000054.py

--- Enrique_27000054.py
+++ Enrique_27000054.py
@@ -5,6 +5,7 @@ Proyecto Polinomio de Lagrange.
 Cada participante debe completar su módulo y luego solicitar el Pull-Request.
 """
 
+
 def polinomio_lagrange(X, Y):
     """
     Implementa y retorna el Polinomio de Lagrange para las listas X e Y.
@@ -14,20 +15,34 @@ def polinomio_lagrange(X, Y):
     Y: lista de valores de la variable dependiente.
     """
 
-     if len(X) != len(Y): raise ValueError("Dimensiones diferentes en X e Y.")
+    if len(X) != len(Y):
+         raise ValueError("Dimensiones diferentes en X e Y.")
 
     # Ordena el par (x, y) en forma ascendente por x.
     pares = list(zip(X, Y))
     pares.sort(key = lambda x: x[0])
     X, Y  = zip(*pares)
-
-     def polinomio(x):
-        pass
-
+    r= len(X)
+ 
+    def polinomio(x):
+        px=0
+        j=[]
+        for i in range (r):
+            j= Y[i]
+            for k in range (r):
+                if (k!=i):
+                    j=j*((x-X[k]) / (X[i]-X[k]))                   
+            px= px+j
+            
+        return px
+        
     return polinomio
 
 
 if __name__ == '__main__':
     # Pruebe aquí su Polinomio de Lagrange ...
-
-    pass
+    X=[-4,-3,2,-6]
+    Y=[-16,-5,-10,-50]
+    polinomio=polinomio_lagrange(X,Y)
+    p=polinomio(1)
+    print(p)
